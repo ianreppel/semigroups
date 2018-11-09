362ab99af4d80ef653bc8a2f890a7ec2906e6c36
Ian Hellstrom <hellstrom@spotify.com>
2018-11-09 09:26:35 +0100

    Disable shrinkage so the output of the generators makes more sense

--- src/test/scala/NotASemigroupSuite.scala
+++ src/test/scala/NotASemigroupSuite.scala
@@ -1,21 +1,27 @@
+import org.scalacheck.Prop.forAllNoShrink
+import org.scalatest.FlatSpec
+import org.scalatest.prop.Checkers.check
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
-import org.scalatest.{FlatSpec, Matchers}
 
-class NotASemigroupSuite extends FlatSpec with Matchers with GeneratorDrivenPropertyChecks {
+class NotASemigroupSuite extends FlatSpec with GeneratorDrivenPropertyChecks {
 
   "NotASemigroup's binary operation" should "be closed" in {
-    forAll((i1: Int, i2: Int) => {
-      val (a1, a2) = (NotASemigroup(i1), NotASemigroup(i2))
-      NotASemigroup.plus(a1, a2) shouldBe a [NotASemigroup]
-    })
+    check {
+      forAllNoShrink((i1: Int, i2: Int) => {
+        val (a1, a2) = (NotASemigroup(i1), NotASemigroup(i2))
+        NotASemigroup.plus(a1, a2).isInstanceOf[NotASemigroup]
+      })
+    }
   }
 
   it should "be associative" in {
-    forAll((i1: Int, i2: Int, i3: Int) => {
-      val (a1, a2, a3) = (NotASemigroup(i1), NotASemigroup(i2), NotASemigroup(i3))
-      val left = NotASemigroup.plus(NotASemigroup.plus(a1, a2), a3)
-      val right = NotASemigroup.plus(a1, NotASemigroup.plus(a2, a3))
-      left shouldEqual right
-    })
+    check {
+      forAllNoShrink((i1: Int, i2: Int, i3: Int) => {
+        val (a1, a2, a3) = (NotASemigroup(i1), NotASemigroup(i2), NotASemigroup(i3))
+        val left = NotASemigroup.plus(NotASemigroup.plus(a1, a2), a3)
+        val right = NotASemigroup.plus(a1, NotASemigroup.plus(a2, a3))
+        left == right
+      })
+    }
   }
 }
